0e83b1c706242d54bc977ee5ede9449fe4842b06
Palak Agarwal <145057590+palakagarwal16@users.noreply.github.com>
2025-06-09 20:27:45 +0530

    factorialwithloop.java

--- factorialwithloop.java
+++ factorialwithloop.java
@@ -1,2 +1,12 @@
 public class factorialwithloop {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(" enter your number  ");
+        int num  = sc.nextInt();
+        int fact = 1;
+        for (int i = 1; i <num; i++) {
+         fact =  ((fact)*(i));
+        }
+            System.out.println(fact);
+    }
 }
