b494dbd61f489f92bdd3d5c192e5407a8a9d58b9
Palak Agarwal <145057590+palakagarwal16@users.noreply.github.com>
2025-06-11 18:03:53 +0530

    Add files via upload

---
+++ pattern5.java
@@ -0,0 +1,13 @@
+public class pattern5 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 7; i++) {
+            for (int j = 0; j < 7-i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 0; k < 2*i - 1; k++) {
+                System.out.print("$");
+            }
+            System.out.println();
+        }
+    }
+}
